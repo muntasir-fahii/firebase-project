33ababaa463331953c9bd2655be08ab67c3cde93
muntasir-fahim <fa3877957@gmail.com>
2023-01-30 03:42:47 +0600

    icon added

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,6 +1,10 @@
+import { FcGoogle } from "react-icons/fc";
+
 const LogIn = () => {
   //google log in system
-  const handleGoogleLogIn = () => {};
+  const handleGoogleLogIn = () => {
+    console.log("clicked");
+  };
   return (
     <div className="login container mx-auto py-10">
       <h2 className="section-title text-center text-4xl font-medium text-gray-200 mb-10">
@@ -10,9 +14,9 @@ const LogIn = () => {
       <div className="login-platforms flex flex-col gap-5 items-center ">
         <button
           onClick={handleGoogleLogIn}
-          className="google-sign-in bg-slate-100 text-md text-slate-800 h-12 w-80 rounded-lg hover:bg-rose-500 hover:text-slate-100 duration-300"
+          className="google-sign-in flex gap-2 items-center justify-center bg-slate-100 text-md text-slate-800 h-12 w-80 rounded-lg hover:bg-rose-500 hover:text-slate-100 duration-300"
         >
-          Login with Google
+          <FcGoogle className="text-lg" /> Login with Google
         </button>
       </div>
     </div>
